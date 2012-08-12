0ecde514657ca06c2bdd58e7f58810e59f07186a
Amirmahdi Brenjian <a.brenjian@1dws.com>
2012-08-12 21:09:33 +0430

    add Analytics!

--- sale.php
+++ sale.php
@@ -5,6 +5,20 @@
 <title>Naghsh Engineering Group</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 <script type="text/javascript" src="script.js" language="javascript"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34051218-1']);
+  _gaq.push(['_setDomainName', 'naghsh-co.ir']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <link href="Images/favicon.ico" type="image/png" rel="shortcut icon" />
 </head>
 
